d9fdeb787008d5475fd7bbb188bdd72e4c905ee9
Cygnux X-1 <62252129+Dekatron322@users.noreply.github.com>
2025-05-16 16:26:07 +0100

    fixed

--- src/components/Modals/SuccessModal.jsx
+++ src/components/Modals/SuccessModal.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import Confirmation from "../../assets/Confirmation.png";
 
 const SuccessModal = () => {
-  return (
-    <div className="modal__background">
-      <div className="modal__containerpp">
-        <img
-          className="modal__success-img"
-          src={Confirmation}
-          alt="confirmation"
-        />
-        <p>Your event has been published</p>
-        <a href="https://usepurplepages.com/events">
-          <button className="personal__input button">Go To Events</button>
-        </a>
-      </div>
-    </div>
-  );
+	return (
+		<div className='modal__background'>
+			<div className='modal__containerpp'>
+				<img
+					className='modal__success-img'
+					src={Confirmation}
+					alt='confirmation'
+				/>
+				<p>Your event has been published</p>
+				<a href='https://www.usepurplepages.com/events'>
+					<button className='personal__input button'>Go To Events</button>
+				</a>
+			</div>
+		</div>
+	);
 };
 
 export default SuccessModal;
